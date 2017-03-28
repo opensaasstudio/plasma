15305fa2b6c1e4595491b6cfb69bd9bb10c29bc8
upamune <jajkeqos@gmail.com>
2017-03-28 16:21:28 +0900

    add a metrics file

---
+++ metrics/metrics.go
@@ -0,0 +1,32 @@
+package metrics
+
+import metrics "github.com/rcrowley/go-metrics"
+
+type Metrics struct {
+	registry metrics.Registry
+}
+
+const (
+	clientCounterName = "counter"
+)
+
+func New() (Metrics, error) {
+	m := Metrics{
+		registry: metrics.NewRegistry(),
+	}
+
+	clientCounter := metrics.NewCounter()
+	if err := m.registry.Register(clientCounterName, clientCounter); err != nil {
+		return m, err
+	}
+
+	return m, nil
+}
+
+func (m *Metrics) IncClientCount() {
+	metrics.GetOrRegisterCounter(clientCounterName, m.registry).Inc(int64(1))
+}
+
+func (m *Metrics) DecClientCount() {
+	metrics.GetOrRegisterCounter(clientCounterName, m.registry).Dec(int64(1))
+}
